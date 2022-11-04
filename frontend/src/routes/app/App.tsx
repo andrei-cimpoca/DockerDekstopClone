99196e050bf92ae9ab2517454292cfa3a1e539f7
Andrei Cimpoca <andrei.cimpoca@nexusgroup.com>
2022-11-04 14:14:11 +0200

    added packaging

--- frontend/src/routes/app/App.tsx
+++ frontend/src/routes/app/App.tsx
@@ -3,22 +3,37 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import MainMenu, {DrawerWidth} from "../../fragments/mainmenu/MainMenu";
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 const drawerWidth = DrawerWidth;
 
 interface AppProps {}
 
 export default function App(props: React.PropsWithChildren<AppProps>) {
+    const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+    const theme = React.useMemo(
+        () =>
+          createTheme({
+            palette: {
+              mode: prefersDarkMode ? 'dark' : 'light',
+            },
+          }),
+        [prefersDarkMode],
+      );
+    
     return (
-        <Box sx={{display: 'flex'}}>
-            <MainMenu/>
-            <Box
-                component="main"
-                sx={{flexGrow: 1, p: 3, width: {sm: `calc(100% - ${drawerWidth}px)`}}}
-            >
-                <Toolbar/>
-                { props.children }
+        <ThemeProvider theme={theme}>
+            <Box sx={{display: 'flex'}}>
+                <MainMenu/>
+                <Box
+                    component="main"
+                    sx={{flexGrow: 1, p: 3, width: {sm: `calc(100% - ${drawerWidth}px)`}}}
+                >
+                    <Toolbar/>
+                    { props.children }
+                </Box>
             </Box>
-        </Box>
+        </ThemeProvider>
     );
 }
